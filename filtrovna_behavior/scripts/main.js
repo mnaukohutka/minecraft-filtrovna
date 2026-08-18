5a56abc9380ccd3f9a63e93a080867c915a2d0d0
mnaukohutka <zpravy.robotu@outlook.cz>
2026-08-18 14:52:25 +0200

    Update main.js

--- filtrovna_behavior/scripts/main.js
+++ filtrovna_behavior/scripts/main.js
@@ -1,37 +1,19 @@
-// main.js — Hlavní entry point (Část 12.1).
-import { world, system } from "@minecraft/server";
-import { loadConfig } from "./modules/config.js";
-import { registerInventoryManager, initLoadedChunks, repairTrackingOnLoad } from "./modules/inventory_manager.js";
-import { registerTickHandler } from "./modules/tick_handler.js";
-import { registerBlockComponents, registerUIHandler } from "./modules/ui_handler.js";
-import { registerCommandHandler } from "./modules/command_handler.js";
-import { registerGolemManager } from "./modules/golem_manager.js";
-import { registerEvents } from "./modules/events.js";
-import { registerMasterManager } from "./modules/master.js";
-import { registerConveyorManager } from "./modules/conveyor.js";
-import { registerSmartHopperManager } from "./modules/smart_hopper.js";
-import { registerDockManager } from "./modules/golem_dock.js";
-import { registerScannerManager } from "./modules/scanner.js";
+import * as ui from "./modules/ui_handler.js";
+import * as storage from "./modules/storage.js";
+import * as config from "./modules/config.js";
+import * as conveyor from "./modules/conveyor.js";
+import * as scanner from "./modules/scanner.js";
+import * as smartHopper from "./modules/smart_hopper.js";
+import * as master from "./modules/master.js";
+import * as golemDock from "./modules/golem_dock.js";
+import * as events from "./modules/events.js";
+import * as tickHandler from "./modules/tick_handler.js";
 
-// Inicializace při startu světa.
-world.afterEvents.worldInitialize.subscribe(() => {
-  loadConfig();
-  registerInventoryManager();
-  initLoadedChunks();
-  repairTrackingOnLoad();
+// Registrace custom componentů
+system.registerComponent("filtrovna:on_interact", ui.handleInteract);
+system.registerComponent("filtrovna:on_interact_hopper", smartHopper.handleInteract);
+system.registerComponent("filtrovna:on_interact_master", master.handleInteract);
+
+system.afterEvents.tick(tick => {
+  tickHandler.onTick(tick);
 });
-
-// Registrace handlerů.
-registerEvents();
-registerBlockComponents();
-registerTickHandler();
-registerUIHandler();
-registerCommandHandler();
-registerGolemManager();
-registerMasterManager();
-registerConveyorManager();
-registerSmartHopperManager();
-registerDockManager();
-registerScannerManager();
-
-console.log("[Filtrovna] Addon načten — verze 1.0.0");
